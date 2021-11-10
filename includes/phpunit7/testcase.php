c4be2c2161bcdf053628a0efb393741e7d7fb6c6
WP <34419856+wp-phpunit-bot@users.noreply.github.com>
2021-11-10 19:49:27 +0000

    Building 5.6.6

--- includes/phpunit7/testcase.php
+++ includes/phpunit7/testcase.php
@@ -1,5 +1,21 @@
 <?php
 
+use Yoast\PHPUnitPolyfills\Helpers\AssertAttributeHelper;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertClosedResource;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertFileDirectory;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertFileEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertionRenames;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertIsType;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertNumericType;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertObjectEquals;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertStringContains;
+use Yoast\PHPUnitPolyfills\Polyfills\EqualToSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectException;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionMessageMatches;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionObject;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectPHPException;
+
 require_once dirname( __DIR__ ) . '/abstract-testcase.php';
 
 /**
@@ -13,28 +29,97 @@ require_once dirname( __DIR__ ) . '/abstract-testcase.php';
  */
 class WP_UnitTestCase extends WP_UnitTestCase_Base {
 
-	/**
-	 * Asserts that two variables are equal (with delta).
-	 *
-	 * This method has been backported from a more recent PHPUnit version,
-	 * as tests running on PHP 5.6 use PHPUnit 5.7.x.
-	 *
-	 * @since 5.6.0
-	 *
-	 * @param mixed  $expected First value to compare.
-	 * @param mixed  $actual   Second value to compare.
-	 * @param float  $delta    Allowed numerical distance between two values to consider them equal.
-	 * @param string $message  Optional. Message to display when the assertion fails.
-	 *
-	 * @throws ExpectationFailedException
-	 * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
-	 */
-	public static function assertEqualsWithDelta( $expected, $actual, float $delta, string $message = '' ): void {
-		$constraint = new PHPUnit\Framework\Constraint\IsEqual(
-			$expected,
-			$delta
-		);
+	use AssertAttributeHelper;
+	use AssertClosedResource;
+	use AssertEqualsSpecializations;
+	use AssertFileDirectory;
+	use AssertFileEqualsSpecializations;
+	use AssertionRenames;
+	use AssertIsType;
+	use AssertNumericType;
+	use AssertObjectEquals;
+	use AssertStringContains;
+	use EqualToSpecializations;
+	use ExpectException;
+	use ExpectExceptionMessageMatches;
+	use ExpectExceptionObject;
+	use ExpectPHPException;
 
-		static::assertThat( $actual, $constraint, $message );
+	/**
+	 * Wrapper method for the `set_up_before_class()` method for forward-compatibility with WP 5.9.
+	 */
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		static::set_up_before_class();
 	}
+
+	/**
+	 * Wrapper method for the `tear_down_after_class()` method for forward-compatibility with WP 5.9.
+	 */
+	public static function tearDownAfterClass() {
+		static::tear_down_after_class();
+		parent::tearDownAfterClass();
+	}
+
+	/**
+	 * Wrapper method for the `set_up()` method for forward-compatibility with WP 5.9.
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->set_up();
+	}
+
+	/**
+	 * Wrapper method for the `tear_down()` method for forward-compatibility with WP 5.9.
+	 */
+	public function tearDown() {
+		$this->tear_down();
+		parent::tearDown();
+	}
+
+	/**
+	 * Wrapper method for the `assert_pre_conditions()` method for forward-compatibility with WP 5.9.
+	 */
+	protected function assertPreConditions() {
+		parent::assertPreConditions();
+		$this->assert_pre_conditions();
+	}
+
+	/**
+	 * Wrapper method for the `assert_post_conditions()` method for forward-compatibility with WP 5.9.
+	 */
+	protected function assertPostConditions() {
+		parent::assertPostConditions();
+		$this->assert_post_conditions();
+	}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	public static function set_up_before_class() {}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	public static function tear_down_after_class() {}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	protected function set_up() {}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	protected function tear_down() {}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	protected function assert_pre_conditions() {}
+
+	/**
+	 * Placeholder method for forward-compatibility with WP 5.9.
+	 */
+	protected function assert_post_conditions() {}
 }
